cdf27e0edc7415d3e91c5471497d750b80094787
kyleseven <kyle.senerpida@icloud.com>
2021-12-07 19:21:22 -0600

    client: make BookDisplay into a functional component

--- client/src/components/BookDisplay.js
+++ client/src/components/BookDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import '../App.css'
 import 'emerald-ui/lib/styles.css';
 import { Link } from 'react-router-dom';
@@ -6,30 +6,33 @@ import Card from 'emerald-ui/lib/Card';
 import CardGrid from 'emerald-ui/lib/CardGrid';
 import axios from 'axios';
 
-class BookDisplay extends React.Component {
-  state = {
-    books: [],
-  };
 
-  componentDidMount() {
+function BookDisplay() {
+  const [books, setBooks] = useState();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
     axios.get("/books", { headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` } })
-    .then(res => {
-      this.setState({ books: res.data })
-    });
+      .then(res => {
+        setBooks(res.data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>
   }
 
-  render() {
-    return (
-      <div style={{ paddingLeft: "40px", paddingRight: "40px" }}>
-        <CardGrid>
-          {this.state.books.map(book => <Card key={book.book_id}>
-            <Link to={"/book/" + book.book_id}><h2>{book.title}</h2></Link><br />
-            <i>by {book.author}</i>
-          </Card>)}
-        </CardGrid>
-      </div>
-    )
-  }
+  return (
+    <div style={{ paddingLeft: "40px", paddingRight: "40px" }}>
+      <CardGrid>
+        {books.map(book => <Card key={book.book_id}>
+          <Link to={"/book/" + book.book_id}><h2>{book.title}</h2></Link><br />
+          <i>by {book.author}</i>
+        </Card>)}
+      </CardGrid>
+    </div>
+  )
 }
 
 export default BookDisplay;
